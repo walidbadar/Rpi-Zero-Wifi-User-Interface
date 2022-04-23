17d0111b1bcb7c096a46479031e2f067dae5fbfa
walidbadar <81442784+walidbadar@users.noreply.github.com>
2022-04-23 05:38:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,11 @@ def landing():
     essid = os.popen('sudo iw wlan0 scan | egrep \'SSID\'', 'r')
     essid = essid.read()
     essid = essid.split('\n')
-    essid = ["this is the 1st wifi", "this is the 2nd wifi", "this is the 3rd wifi"]
     return render_template('index.html', essid = essid)
 
 @app.route("/connect", methods=["POST"])
 def repeaterConf():    
     ssid = request.form['essid']
-    print(ssid)
     password = request.form['pass']
     scheme = connect.SchemeWPA('wlan1', ssid, {"ssid": ssid,"psk": password})
     scheme.save()
